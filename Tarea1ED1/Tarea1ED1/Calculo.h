d4fa39715b1b4a3f7f874c1d37e1a676f4856ca3
Isabel Galeano <isagaleano9@gmail.com>
2021-05-13 23:46:13 -0600

    Update separacion de clases

---
+++ Tarea1ED1/Tarea1ED1/Calculo.h
@@ -0,0 +1,14 @@
+#pragma once
+class Calculo
+{
+
+public:
+	Calculo();
+	int mayor(int a, int b, int c);
+	double promedio(int n);
+	int numFactorial(int n);
+	double notaEstudiante(double n);
+	int numBinario(long long n);
+	int numDigitos(int n);
+};
+
--- Tarea1ED1/Tarea1ED1/Tarea1ED1.cpp
+++ Tarea1ED1/Tarea1ED1/Tarea1ED1.cpp
@@ -3,75 +3,7 @@
 
 #include <iostream>
 using namespace std;
-
-class Calculo {
-public: 
-    static int mayor(int a, int b, int c) {
-        if (a > b && a > c) {
-            cout << "El numero mayor es: " << a;
-        }
-        if (b > c && b > a) {
-            cout << "El numero mayor es: " << b;
-        }
-        if (c > b && c > a) {
-            cout << "El numero mayor es: " << c;
-        }
-        return 0;
-
-    }
-
-    static double promedio(int n) {
-        double numero, promedio;
-        double total = 0;
-        for (int i = 0; i < n; i++) {
-            cout << "Ingrese el numero: "; cin >> numero;
-            total += numero;
-        }
-        promedio = total / n;
-      
-        return promedio;
-    }
-
-    static int numFactorial(int n) {
-        int factorial = 1;
-        for (int i = 1; i <= n; i++) {
-            factorial *= i;
-        }
-
-        
-        return factorial;
-    }
-
-    static double notaEstudiante(double n) {
-        double nota = n / 50;
-        nota = nota * 100;
-        return nota;
-    }
-
-    static int numBinario(long long n) {
-        int numeroDecimal = 0, i = 0, calculo;
-        while (n != 0)
-        {
-            calculo = n % 10;
-            n /= 10;
-            numeroDecimal += calculo * pow(2, i);
-            ++i;
-        }
-        return numeroDecimal;
-    }
-
-    static int numDigitos(int n) {
-        unsigned int numeroDigitos = 0;
-        do {
-            ++numeroDigitos;
-            n /= 10;
-        } while (n);
-
-        return numeroDigitos;
-
-    }
-
-};
+#include "Calculo.h"
 int main()
 {
     int a, b, c;
